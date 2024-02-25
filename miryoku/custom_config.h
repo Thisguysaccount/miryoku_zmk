1798b102ce6c85a70e56a596671d7a89261e4983
Thisguysaccount <161144198+Thisguysaccount@users.noreply.github.com>
2024-02-24 21:16:33 -0500

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,15 +1,2 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-
-# Enables a combination of settings that are planned to be default in future versions of ZMK to improve connection stability.
-# This includes changes to timing on BLE pairing initation, restores use of the updated/new LLCP implementation, and disables 2M PHY support.
-CONFIG_ZMK_BLE_EXPERIMENTAL_CONN=y
-
-# Enable deep sleep support
-CONFIG_ZMK_SLEEP=y
-
-# Milliseconds of inactivity before entering deep sleep
-CONFIG_ZMK_IDLE_SLEEP_TIMEOUT=360000
-
-# The name of the keyboard (max 16 characters)
-CONFIG_ZMK_KEYBOARD_NAME="Corne view"
